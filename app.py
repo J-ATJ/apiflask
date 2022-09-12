3d6507de17806030323521f88032fa5580b75f41
J-ATJ <jyoruge6798@gmail.com>
2022-09-12 01:47:40 -0500

    Added route /<string:driverId>/short/pretty/ and templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 from db import Drivers as drivers
 
@@ -23,6 +23,19 @@ def get_driver_short(driverId):
                 f"<h1>{driver['givenName']} {driver['familyName']}</h1><hr><h4>Number: {driver['permanentNumber']}</h4><h4>Nationality: {driver['nationality']}</h4>"
             )
     return '<h1>No driver with that id</h1>'
+
+@app.route('/<string:driverId>/short/pretty/')
+def get_driver_short_pretty(driverId):
+    for driver in drivers:
+        if driver['driverId'] == driverId:
+            return render_template('driver.html', 
+                givenName = driver['givenName'],
+                familyName = driver['familyName'],
+                permanentNumber = driver['permanentNumber'],
+                nationality = driver['nationality']
+            )
+    return  render_template('notFound.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)  
