a207e1abe712f7790ee1abf2a238f6c6064485e8
J-ATJ <jyoruge6798@gmail.com>
2022-09-12 00:53:06 -0500

    Added route /<string:driverId/short/

--- app.py
+++ app.py
@@ -8,14 +8,21 @@ app = Flask(__name__)
 def get_index():
     return jsonify({'Drivers': drivers})
 
-@app.route('/<string:driverId>')
+@app.route('/<string:driverId>/')
 def get_driver(driverId):
     for driver in drivers:
         if driver['driverId'] == driverId:
             return jsonify(driver)
     return '<h1>No driver with that id</h1>'
     
-
+@app.route('/<string:driverId>/short/')
+def get_driver_short(driverId):
+    for driver in drivers:
+        if driver['driverId'] == driverId:
+            return (
+                f"<h1>{driver['givenName']} {driver['familyName']}</h1><hr><h4>Number: {driver['permanentNumber']}</h4><h4>Nationality: {driver['nationality']}</h4>"
+            )
+    return '<h1>No driver with that id</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)  
